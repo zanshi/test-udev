6d7e1be5f312777dd1b25ff62604a72f52e8fd8a
Niclas Olmenius <niclas@voysys.se>
2021-10-19 16:18:16 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -40,19 +40,37 @@ fn read_lvm_slave_name(lvm_disk_slaves_dir: String) -> Result<String, LicenseSer
     Ok(lvm_physical_disk_name)
 }
 
-fn get_lvm_device_serial(mount: Mount, root_disk: Disk) -> Result<String, LicenseSerialError> {
-    let root_mount_info = &mount.info;
-    let root_device_id = root_mount_info.dev;
+fn get_device_serial_from_name(name: &str) -> Result<String, LicenseSerialError> {
+    let mut enumerator = udev::Enumerator::new().unwrap();
+    enumerator.match_subsystem("block").unwrap();
 
+    let devices = enumerator
+        .scan_devices()
+        .map_err(|e| LicenseSerialError::UdevDeviceScanFailure(e))?;
+
+    let device = devices
+        .into_iter()
+        .filter(|d| d.sysname().to_string_lossy() == name)
+        .next()
+        .ok_or_else(|| LicenseSerialError::UdevDeviceNotFound)?;
+
+    let serial = device
+        .property_value("ID_SERIAL_SHORT")
+        .or_else(|| device.property_value("ID_SERIAL"))
+        .map(|s| s.to_string_lossy().to_string())
+        .ok_or_else(|| LicenseSerialError::SerialNotFound)?;
+
+    Ok(serial)
+}
+
+fn get_lvm_device_serial(root_disk: Disk) -> Result<String, LicenseSerialError> {
     let disk_name = &root_disk.name;
 
     let lvm_disk_slaves_dir = format!("/sys/block/{}/slaves", disk_name);
 
     let lvm_physical_disk_name = read_lvm_slave_name(lvm_disk_slaves_dir)?;
 
-    // println!("{:?}", slave);
-
-    Ok("".to_string())
+    get_device_serial_from_name(&lvm_physical_disk_name)
 }
 
 fn get_regular_device_serial(mount: Mount) -> Result<String, LicenseSerialError> {
@@ -66,26 +84,7 @@ fn get_regular_device_serial(mount: Mount) -> Result<String, LicenseSerialError>
         .find_by_id(root_device_id)
         .ok_or_else(|| LicenseSerialError::BlockDeviceNotFound)?;
 
-    let mut enumerator = udev::Enumerator::new().unwrap();
-    enumerator.match_subsystem("block").unwrap();
-
-    let devices = enumerator
-        .scan_devices()
-        .map_err(|e| LicenseSerialError::UdevDeviceScanFailure(e))?;
-
-    let device = devices
-        .into_iter()
-        .filter(|d| d.sysname().to_string_lossy() == block_device.name)
-        .next()
-        .ok_or_else(|| LicenseSerialError::UdevDeviceNotFound)?;
-
-    let serial = device
-        .property_value("ID_SERIAL_SHORT")
-        .or_else(|| device.property_value("ID_SERIAL"))
-        .map(|s| s.to_string_lossy().to_string())
-        .ok_or_else(|| LicenseSerialError::SerialNotFound)?;
-
-    Ok(serial)
+    get_device_serial_from_name(&block_device.name)
 }
 
 fn get_device_serial() -> Result<String, LicenseSerialError> {
@@ -104,7 +103,7 @@ fn get_device_serial() -> Result<String, LicenseSerialError> {
         .ok_or_else(|| LicenseSerialError::RootDiskNotFound)?;
 
     if root_disk.lvm {
-        get_lvm_device_serial(root_mount, root_disk)
+        get_lvm_device_serial(root_disk)
     } else {
         get_regular_device_serial(root_mount)
     }
